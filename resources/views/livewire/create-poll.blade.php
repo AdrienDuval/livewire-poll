9cb3219164f1f8cbc23022ec232d07f07a27db23
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2023-09-21 00:19:27 +0530

    Creating the poll and option functionality
    Creating all the function and methods for effectively storing the polls and options as a relationship in the database.

--- resources/views/livewire/create-poll.blade.php
+++ resources/views/livewire/create-poll.blade.php
@@ -1,10 +1,25 @@
 <div>
-    <form >
+    <form wire:submit.prevent="createPoll">
         <label>Poll Title </label>
 
-        <input type="text" wire:model="title" placeholder="{{$title}}" />
-        
-        Current Title : {{ $title }}
-@dd($title);
+        <input type="text" wire:model.live="title" placeholder="{{ $title }}" />
+
+        <div class="mb-4">
+            <button class="btn mt-4" wire:click.prevent="addOption">Add Option</button>
+        </div>
+        <div class="">
+            @foreach ($options as $index => $option)
+                <div class="mt-4">
+                    <label>Option {{$index + 1}}</label>
+                    <div class="flex gap-2">
+                        <input type="text" wire:model.live="options.{{$index}}" />
+                        <button class="btn" wire:click.prevent="removeOption({{$index}})">Remove</button>
+                    </div>
+                </div>
+
+                
+            @endforeach
+        </div>
+        <button type="submit" class="btn mt-4">Create Poll</button>
     </form>
 </div>
